f6294cea21a5aa47ccef0ab3d730f835f1c7b0c0
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-09-16 20:17:10 +0500

    added inKell so that I can navigate to the other sreen

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_heart/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
@@ -12,20 +13,31 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(0.7),
-            color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      onTap: () => Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (_) {
+            return CategoryMealsScreen();
+          },
         ),
-        borderRadius: BorderRadius.circular(15),
       ),
-      child: Text(title, style: Theme.of(context).textTheme.displayLarge),
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(0.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Text(title, style: Theme.of(context).textTheme.displayLarge),
+      ),
     );
   }
 }
