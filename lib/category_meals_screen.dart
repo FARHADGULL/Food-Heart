15abc58848389504a3341d46be0a35ffb40b5904
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-09-17 18:47:09 +0500

    Update category_meals_screen.dart

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_heart/dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category_meals';
@@ -15,14 +16,18 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeal = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryId);
+    });
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle!),
       ),
       body: ListView.builder(
         itemBuilder: ((context, index) {
-          return;
+          return Text(categoryMeal.elementAt(index).title);
         }),
+        itemCount: categoryMeal.length,
       ),
     );
   }
