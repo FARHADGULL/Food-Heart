3cbc76b451e1b44fd3f0e4fd7b1a28069cfc6ef1
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-09-17 18:57:54 +0500

    Update category_meals_screen.dart

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -16,9 +16,11 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+
     final categoryMeal = DUMMY_MEALS.where((meal) {
       return meal.categories.contains(categoryId);
-    });
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle!),
