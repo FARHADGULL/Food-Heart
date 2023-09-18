e474c509e4cec1578314ff6553a2dea4c99bcc68
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-09-18 17:16:44 +0500

    Update tabs_screen.dart

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -48,3 +48,47 @@ class _TabsScreenState extends State<TabsScreen> {
     );
   }
 }
+
+//Following code is for tabs with top navigation bar i-e tabBar and TabBarView widgets. 
+
+// import 'package:flutter/material.dart';
+// import 'package:food_heart/screens/categories_screen.dart';
+// import 'package:food_heart/screens/favorites_screen.dart';
+
+// class TabsScreen extends StatefulWidget {
+//   const TabsScreen({super.key});
+
+//   @override
+//   State<TabsScreen> createState() => _TabsScreenState();
+// }
+
+// class _TabsScreenState extends State<TabsScreen> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return DefaultTabController(
+//       length: 2,
+//       child: Scaffold(
+//           appBar: AppBar(
+//             title: const Text('Food Heart'),
+//             bottom: const TabBar(
+//               tabs: [
+//                 Tab(
+//                   icon: Icon(Icons.category),
+//                   text: 'Categories',
+//                 ),
+//                 Tab(
+//                   icon: Icon(Icons.star),
+//                   text: 'Favorites',
+//                 )
+//               ],
+//             ),
+//           ),
+//           body: const TabBarView(
+//             children: [
+//               CategoriesScreen(),
+//               FavoritesScreen(),
+//             ],
+//           )),
+//     );
+//   }
+// }
