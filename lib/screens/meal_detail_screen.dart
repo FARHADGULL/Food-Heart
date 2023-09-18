cad49812aa71b45d6b8f7b2aa11e7c9926924b60
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-09-18 12:52:07 +0500

    Update meal_detail_screen.dart

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -5,6 +5,33 @@ class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal_detail';
   const MealDetailScreen({super.key});
 
+  Widget buildSectionTitle(BuildContext context, String text) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Text(
+        text,
+        style: Theme.of(context).textTheme.displayLarge,
+      ),
+    );
+  }
+
+  Widget buildContainer(Widget child) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(
+          color: Colors.grey,
+        ),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.all(10),
+      height: 200,
+      width: 300,
+      child: child,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context)?.settings.arguments as String;
@@ -16,50 +43,52 @@ class MealDetailScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(selectedItem.title),
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 300,
-            width: double.infinity,
-            child: Image.network(
-              selectedItem.imageUrl,
-              fit: BoxFit.cover,
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(vertical: 10),
-            child: Text(
-              'Ingrediants',
-              style: Theme.of(context).textTheme.displayLarge,
-            ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(
-                color: Colors.grey,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                selectedItem.imageUrl,
+                fit: BoxFit.cover,
               ),
-              borderRadius: BorderRadius.circular(10),
             ),
-            padding: const EdgeInsets.all(10),
-            margin: const EdgeInsets.all(10),
-            height: 200,
-            width: 300,
-            child: ListView.builder(
-              itemBuilder: (ctx, index) => Card(
-                color: Theme.of(context).hintColor,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: 5,
-                    horizontal: 10,
+            buildSectionTitle(context, 'Ingrediants'),
+            buildContainer(
+              ListView.builder(
+                itemBuilder: (ctx, index) => Card(
+                  color: Theme.of(context).hintColor,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 10,
+                    ),
+                    child: Text(selectedItem.ingredients[index]),
                   ),
-                  child: Text(selectedItem.ingredients[index]),
                 ),
+                itemCount: selectedItem.ingredients.length,
               ),
-              itemCount: selectedItem.ingredients.length,
             ),
-          )
-        ],
+            buildSectionTitle(context, 'Steps'),
+            buildContainer(
+              ListView.builder(
+                itemBuilder: (ctx, index) => Column(
+                  children: [
+                    ListTile(
+                      leading: CircleAvatar(
+                        child: Text('# ${index + 1}'),
+                      ),
+                      title: Text(selectedItem.steps[index]),
+                    ),
+                    const Divider(),
+                  ],
+                ),
+                itemCount: selectedItem.steps.length,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
