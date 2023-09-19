ab7a1075b254f654452c034757ba4aba76f89799
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2023-09-19 15:41:54 +0500

    Update filter_screen.dart

--- lib/screens/filter_screen.dart
+++ lib/screens/filter_screen.dart
@@ -1,20 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:food_heart/widgets/main_drrawer.dart';
 
-class FiltersScreen extends StatelessWidget {
+class FiltersScreen extends StatefulWidget {
   static const routeName = 'filters-screen';
   const FiltersScreen({super.key});
 
   @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  var _isGlutenFree = false;
+  var _isLactoseFree = false;
+  var _isVegan = false;
+  var _isVegetarian = false;
+
+  Widget _buildSwitchListTile(String title, String description,
+      bool currentValue, Function updateValue) {
+    return SwitchListTile(
+      value: currentValue,
+      onChanged: (newValue) => updateValue(newValue),
+      title: Text(title),
+      subtitle: Text(description),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Filters'),
-      ),
-      drawer: MainDrawer(),
-      body: Center(
-        child: Text('Filters!'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Your Filters'),
+        ),
+        drawer: MainDrawer(),
+        body: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Center(
+                child: Text(
+                  'Adjust your meal selection',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  _buildSwitchListTile('Gluten-Free',
+                      'Only include gluten-free meals', _isGlutenFree, (value) {
+                    setState(() {
+                      _isGlutenFree = value;
+                    });
+                  }),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
